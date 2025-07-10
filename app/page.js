727f23ec5a25a096d0af08f21666b83e10d32b55
Ahmadx2025 <ahmadkhoja2023@gmail.com>
2025-07-10 15:54:33 +0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import HighQualityProducts from "./components/HighQualityProducts";
+import MediaUpdates from "./components/MediaUpdates";
+
+export default function Home() {
+  return (
+    <main>
+      <HighQualityProducts />
+      <MediaUpdates />
+    </main>
+  );
+}
